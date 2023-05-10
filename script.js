31b1a2528e6c75cf3c2e9e8973c0d4e5f415f97c
rumana-aafrin <70215401+rumana-aafrin@users.noreply.github.com>
2023-05-10 23:21:00 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+//Script for onclick alert text
+const languageButton = document.querySelector('#language-button');
+languageButton.addEventListener('click', () => {
+  alert('Did you know that I once rode my bike across the entire city?');
+});
